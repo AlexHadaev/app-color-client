59f504194a001248e93db4c9ac8e75fe734d0aa5
Alexandr <alex.hadaev@n3wnormal.com>
2022-11-10 11:03:48 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React, {createContext} from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import ColorStore from "./store/ColorStore";
+import './Index.css';
+
+export const Context = createContext(null as any)
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+    <Context.Provider value = {{
+        color: new ColorStore()
+    }}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Context.Provider>
+
+);
+
