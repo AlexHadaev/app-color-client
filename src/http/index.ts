5b5efb21d5dfd0a8fbf90d3f36d3520dc4479b0e
Alexandr <alex.hadaev@n3wnormal.com>
2022-11-11 17:06:47 +0100

    reviewed code

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === 'production'?process.env.REACT_APP_API_URL_PRODUCTION : process.env.REACT_APP_API_URL
-console.log(process.env.NODE_ENV, ': ', baseURL);
+const baseURL = window.location.hostname !== 'localhost'?process.env.REACT_APP_API_URL_PRODUCTION : process.env.REACT_APP_API_URL
+console.log(window.location.hostname, ': ', baseURL);
 export const $host = axios.create({
     baseURL: baseURL || 'http://localhost:5000/'
 })
